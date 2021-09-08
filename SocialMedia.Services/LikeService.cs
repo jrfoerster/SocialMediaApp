2f12f3279a9ff52008a21545d656fdc7b1785be8
onedayherenow@gmail.com <onedayherenow@gmail.com>
2021-09-07 20:51:01 -0400

    4.1

---
+++ SocialMedia.Services/LikeService.cs
@@ -0,0 +1,39 @@
+﻿using SocialMedia.Models;
+using SocialMedia.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Services
+{
+	public class LikeService
+	{
+		private readonly Guid _userId;
+
+		public LikeService(Guid userId)
+		{
+			_userId = userId;
+		}
+
+		// POST -- CREATE
+		public bool CreateLike(LikeCreate model)
+		{
+			var entity =
+				new Like()
+				{
+					OwnerId = _userId,
+					PostId = model.PostId,
+					Post = model.Post,
+					Id = model.Id
+				};
+
+			using (var ctx = new ApplicationDbContext())
+			{
+				ctx.Likes.Add(entity);
+				return ctx.SaveChanges() == 1;
+			}
+		}
+	}
+}
