249f0aa7cdff1dbc7a82fca0cb55d46a1a21a670
Alexa Hurt <alexacorinnehurt@gmail.com>
2021-09-11 10:08:31 -0400

    add required tags to post models

--- SocialMedia.Models/PostCreate.cs
+++ SocialMedia.Models/PostCreate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace SocialMedia.Models
 {
     public class PostCreate
     {
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Text { get; set; }
     }
 }
--- SocialMedia.Models/PostUpdate.cs
+++ SocialMedia.Models/PostUpdate.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,13 @@ namespace SocialMedia.Models
 {
     public class PostUpdate
     {
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Text { get; set; }
+
+        [Required]
         public int PostId { get; set; }
     }
 }
